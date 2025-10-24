111d774956f1dc0b2c57672a83362b8020a78d8a
mehmet-furkan <mehmetfurkan.akyar@gmail.com>
2025-10-24 17:45:23 +0300

    Gorev 1 tamamlandi

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,56 @@
+/*
+ * Ad Soyad: Mehmet Furkan AKYAR
+ * Ogrenci No: 240541056
+ * Tarih: 10/24/2025
+ * Aciklama: Geometrik Hesap
+ */
+
+
+import java.util.Scanner;
+
+public class GeometrikHesap {
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        //yarıçap bilgisi al
+
+        System.out.println("=== GEOMETRİK HESAPLAYİCİ ===");
+        System.out.print("Daire yaricapini giriniz(cm): ");
+        double yaricap = input.nextDouble();
+
+        //Pi tanımla
+        float pi = 3.1415f;
+
+
+        System.out.println("SONUCLAR: ");
+        System.out.println("----------------");
+        //matematiksel işlemler
+        double alan = pi * Math.pow(yaricap,2);
+        System.out.printf("\nDaire alani: %.2f cm^2", alan);
+
+
+        double cevre = 2 * pi * yaricap;
+        System.out.printf("\nDaire cevre: %.2f cm", cevre);
+
+
+        double cap = 2 * yaricap;
+        System.out.printf("\nDaire capı: %.2f cm", cap);
+
+
+        double kure_hacmi = 4.0/3.0 * Math.pow(yaricap, 3);
+        System.out.printf("\nKure hacmi: %.2f cm^3", kure_hacmi);
+
+
+        double kure_yuzey_alani = 4 * pi *  yaricap * yaricap;
+        System.out.printf("\nKure yuzey alani: %.2f cm^2", kure_yuzey_alani);
+
+
+        input.close();
+
+
+
+    }
+}
 
--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,60 +1,57 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: Mehmet Furkan AKYAR
+ * Ogrenci No: 240541056
+ * Tarih: 10/24/2025
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
  * duzenli bir formatta ekrana yazdirir.
- * Diğer java dosyalarının başında da bu örnek formattaki gibi kısa bilgi giriniz
  */
 
 import java.util.Scanner;
 
 public class OgrenciBilgi {
     public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
-        System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
-        
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
 
-        
-        
-        // Scanner'i kapatin (önemli pratik)
-        
+        // Değişkenler alınıyor
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Adinizi Girin: ");
+        String ad = input.nextLine();
+
+        System.out.println("Soyadinizi Girin: ");
+        String soyad = input.nextLine();
+
+        System.out.println("Ogrenci numaraniz: ");
+        int ogrenci_no = input.nextInt();
+
+        System.out.println("Yasiniz: ");
+        int yas = input.nextInt();
+
+        System.out.println("GPA(0.00-4.00): ");
+        float gpa = input.nextFloat();
+
+        // Başarı durumunu belirlemek için bir değişken oluşturalım
+        String durum;
+
+        // GPA'ya göre durumu belirleyelim
+        if (gpa >= 2.00) {
+            durum = "Basarili Ogrenci";
+        } else {
+            durum = "Basarisiz Ogrenci";
+        }
+
+
+        System.out.println("===OGRENCİ BİLGİ SİSTEMİ===");
+        System.out.printf("Ad Soyad: %s %s", ad, soyad);
+        System.out.printf("\nOgrenci No: %d", ogrenci_no);
+        System.out.printf("\nYaş: %d", yas);
+        System.out.printf("\nGPA: %.2f", gpa);
+        System.out.printf("\nDurum: %s", durum);
+
+
+        input.close();
+
     }
 }
